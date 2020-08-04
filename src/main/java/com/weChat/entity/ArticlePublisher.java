b075cb0085a3aa32aecc6e1869197cb0ba62e37b
1715924016 <1315661172@qq.com>
2020-08-04 15:01:29 +0800

    1、自定义发返回数据结构类型（JSONResult）和Object对象数据判断工具类； 2、实体Article对象数据库字段改变；

---
+++ src/main/java/com/weChat/entity/ArticlePublisher.java
@@ -0,0 +1,105 @@
+package com.weChat.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "article_publisher")
+public class ArticlePublisher {
+    /**
+     * 表主键 - uuid
+     */
+    @Id
+    @Column(name = "PUBLISHER_UUID")
+    private String publisherUuid;
+
+    /**
+     * 发布者名称
+     */
+    @Column(name = "PUBLISHER_NAME")
+    private String publisherName;
+
+    /**
+     * 发布者头像路径
+     */
+    @Column(name = "PUBLISHER_IMAGE")
+    private String publisherImage;
+
+    /**
+     * 发布者状态（1:启用，0:停用），停用时不可发布文章且之前文章不给予展示
+     */
+    @Column(name = "STATUS")
+    private String status;
+
+    /**
+     * 获取表主键 - uuid
+     *
+     * @return PUBLISHER_UUID - 表主键 - uuid
+     */
+    public String getPublisherUuid() {
+        return publisherUuid;
+    }
+
+    /**
+     * 设置表主键 - uuid
+     *
+     * @param publisherUuid 表主键 - uuid
+     */
+    public void setPublisherUuid(String publisherUuid) {
+        this.publisherUuid = publisherUuid == null ? null : publisherUuid.trim();
+    }
+
+    /**
+     * 获取发布者名称
+     *
+     * @return PUBLISHER_NAME - 发布者名称
+     */
+    public String getPublisherName() {
+        return publisherName;
+    }
+
+    /**
+     * 设置发布者名称
+     *
+     * @param publisherName 发布者名称
+     */
+    public void setPublisherName(String publisherName) {
+        this.publisherName = publisherName == null ? null : publisherName.trim();
+    }
+
+    /**
+     * 获取发布者头像路径
+     *
+     * @return PUBLISHER_IMAGE - 发布者头像路径
+     */
+    public String getPublisherImage() {
+        return publisherImage;
+    }
+
+    /**
+     * 设置发布者头像路径
+     *
+     * @param publisherImage 发布者头像路径
+     */
+    public void setPublisherImage(String publisherImage) {
+        this.publisherImage = publisherImage == null ? null : publisherImage.trim();
+    }
+
+    /**
+     * 获取发布者状态（1:启用，0:停用），停用时不可发布文章且之前文章不给予展示
+     *
+     * @return STATUS - 发布者状态（1:启用，0:停用），停用时不可发布文章且之前文章不给予展示
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * 设置发布者状态（1:启用，0:停用），停用时不可发布文章且之前文章不给予展示
+     *
+     * @param status 发布者状态（1:启用，0:停用），停用时不可发布文章且之前文章不给予展示
+     */
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+}
