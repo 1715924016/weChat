8b9f117c3318d880f03f747689594ff7e1054358
1715924016 <1315661172@qq.com>
2020-08-22 13:11:26 +0800

    uploadPhoto method and dependent code!

---
+++ src/main/java/com/weChat/staticsController/UploadHTMLController.java
@@ -0,0 +1,25 @@
+package com.weChat.staticsController;
+
+import com.weChat.controller.ArticleController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tk.mybatis.mapper.annotation.RegisterMapper;
+
+@Controller
+@RequestMapping("/uploadHTML")
+public class UploadHTMLController {
+
+    /**
+     * 日志
+     */
+    private static Logger logger = LoggerFactory.getLogger(ArticleController.class);
+
+    @GetMapping("/uploadPhotoHTML")
+    public String uploadPhotoHTML(){
+        logger.info("进入上传界面！");
+        return "/upload";
+    }
+}
